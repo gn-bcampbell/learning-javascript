b7ccf5291dabf3756fb54f05cd0ff1b5f0211b76
b.campbell787 <b.campbell787@gmail.com>
2022-06-11 17:15:13 +0100

    S2 | EP21 : Truthy & Falsy

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
+    S2 | EP20 : Type Conversion & Coercion
+*/
+
 // Setup
 const inputYear = '1991';
 console.log(inputYear + 18); // 199118
@@ -14,3 +18,24 @@ console.log('I am ' + 23 + ' years old'); // string concat
 console.log('23' - '10' - 3) // 10
 console.log('23' * '2'); // 46
 console.log('23' / '2'); // 11.5
+
+
+/*
+    S2 | EP21 : Truthy & Falsy
+*/
+
+// 5 values are always considered falsy
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all")
+} else {
+    console.log("Get a job you schmuck") //returned as money converted to false
+}
+
