a18296d795a017145b11a3c52634c1f2883b97f2
b.campbell787 <b.campbell787@gmail.com>
2022-06-11 17:07:51 +0100

    S2 | E20 | Type Conversion & Coercion

---
+++ script.js
@@ -0,0 +1,16 @@
+// Setup
+const inputYear = '1991';
+console.log(inputYear + 18); // 199118
+
+// Conversion
+console.log(Number(inputYear)); // 1991
+console.log(Number(inputYear) + 18) // 2009
+console.log(Number('temp')); // NaN
+console.log(String(23)); // '23'
+
+
+// Coercion
+console.log('I am ' + 23 + ' years old'); // string concat
+console.log('23' - '10' - 3) // 10
+console.log('23' * '2'); // 46
+console.log('23' / '2'); // 11.5
