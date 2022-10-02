c8f7b897f29708430f3acdddeb3b63e3c680b7d0
Brendan <b.campbell787@gmail.com>
2022-10-02 19:33:17 +0100

    S3 | EP45: Coding Challenge #3

---
+++ section-three/challenges/3/script.js
@@ -0,0 +1,51 @@
+/*
+    ? Coding Challenge #2
+    
+    * Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+    * implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
+    
+    Your tasks:
+        Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+        tall.
+*/
+
+// ?  1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+// ?  2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method
+const mark = {
+    firstName: 'Mark',
+    lastName: 'Miller',
+    weight: 78,
+    height: 1.69,
+    getFullName: function() {
+        this.fullName =  this.firstName + ' ' + this.lastName;
+        return this.fullName;
+    },
+    calcBMI: function() {
+        this.BMI = this.weight / this.height ** 2;
+        return this.BMI.toFixed(2);
+    }
+}
+
+const john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    weight: 92,
+    height: 1.95,
+    getFullName: function() {
+        this.fullName =  this.firstName + ' ' + this.lastName;
+        return this.fullName;
+    },
+    calcBMI: function() {
+        this.BMI = this.weight / this.height ** 2;
+        return this.BMI.toFixed(2);
+    }
+}
+
+// ?         3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+if (mark.calcBMI() > john.calcBMI())
+    console.log(`${mark.firstName}'s BMI (${mark.calcBMI()}) is higher than ${john.firstName}'s (${john.BMI.toFixed(2)})`)
+else
+    console.log(`${john.firstName}'s BMI (${john.calcBMI()}) is higher than ${mark.firstName}'s (${mark.calcBMI()})`)
+
+console.log(`${mark.firstName} weighs ${mark.weight}kg and is ${mark.height}m tall. ${john.firstName} weighs ${john.weight}kg and is ${john.height}m tall.`)
+
