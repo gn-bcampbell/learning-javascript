4fe900d1cb564a3a9ddd150afb8a9d98df335af4
Brendan <brendan.campbell@paconsulting.com>
2022-10-04 16:25:50 +0100

    S4 | EP53-61: Configure Dev environment

--- section-three/strictMode.js
+++ section-three/strictMode.js
@@ -4,13 +4,17 @@
     --> Forbids certain things
     --> Creates visible errors in dev console
 */
-'use strict';
+"use strict";
 
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) undeclaredVariable = true; // results in not defined error
 
-if (hasDriversLicense) console.log('I can drive.');
+if (hasDriversLicense) console.log("I can drive.");
 
-const interface = 'Audio' //potential javascript reserved word
+const interface = "Audio"; //potential javascript reserved word
+
+const x = 23;
+
+console.log();
