d74b247eedf539074394d7e7922e3d3e5a6af4a5
Brendan <brendan.campbell@paconsulting.com>
2022-10-05 17:03:15 +0100

    S7 | EP 68-75: Dom manipulation, handling on click events, manipulating CSS styles

--- S7 | dom-and-events-fundamentals/projects/guess-my-number/starter/script.js
+++ S7 | dom-and-events-fundamentals/projects/guess-my-number/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// generate secret number
+const secret = Math.floor(Math.random() * 20) + 1;
+console.log(secret);
+
+let score = 20;
+
+// add event listeners
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (score === 1) {
+    document.querySelector('.message').textContent = 'Game over! ❌';
+    document.querySelector('.score').textContent = 0;
+    return;
+  }
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number! ⛔️';
+  } else if (guess > secret) {
+    document.querySelector('.message').textContent = 'Too high 📈';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secret) {
+    document.querySelector('.message').textContent = 'Too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess === secret) {
+    document.querySelector('.message').textContent = 'Correct! 🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347'; //update css
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secret;
+  }
+});
+
+// updating css,
+//  use the ('body').style on query selector
+//  css properties use camelCase in js.
+//  the values used (30rem) must be set as strings
