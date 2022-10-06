7ce4b8702906169241788a16b264113ac9fbe14e
Brendan <brendan.campbell@paconsulting.com>
2022-10-06 12:33:55 +0100

    S7 | Completed project #2: Alter CSS, use click events and keydown events to change css classes

--- S7 | dom-and-events-fundamentals/projects/modal/starter/script.js
+++ S7 | dom-and-events-fundamentals/projects/modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const hideModalContent = function hideModalContent() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModalContent = function showModalContent() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', showModalContent);
+}
+
+btnCloseModal.addEventListener('click', hideModalContent);
+overlay.addEventListener('click', hideModalContent);
+
+document.querySelector('body').addEventListener('keydown', event => {
+  if (event.key === 'Escape') hideModalContent();
+});
