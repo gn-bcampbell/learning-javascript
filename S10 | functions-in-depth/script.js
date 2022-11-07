aeebac54de99a6fccb5bd3bcd0a2f2544181fd30
Brendan <brendan.campbell@paconsulting.com>
2022-11-07 15:58:19 +0000

    S10 | EP 131: Callback functions

--- S10 | functions-in-depth/script.js
+++ S10 | functions-in-depth/script.js
@@ -55,13 +55,53 @@ console.log(jonas); // updates to Mr. Jonas Schmet
 
     ! Higher Order-Function: A function that receives another function as an argument, that returns a new function (or both)
 */
-const addEx = (a, b) => a + b; //storing functions in variables
+const addEx = (a, b) => a + b; //*storing functions in variables
 const counterEx = {
   value: 23,
   inc: function () {
     this.value;
-  }, //storing functions in properties
+  }, //*storing functions in properties
 };
-btnClose.addEventlistener('click', counterEx); //pass functions are arguments in other functions
+// btnClose.addEventlistener('click', counterEx); //* pass functions are arguments in other functions
 
-counterEx.inc.bind(someOtherObject); // return functions from functions or call methods on functions
+// counterEx.inc.bind(someOtherObject); // *return functions from functions or call methods on functions
+
+/*
+    ! S10 | EP 131: Functions Accepting Callback Functions
+
+    ! Callback function, we do not call the function ourselves, we tell one function 
+    ! to call back to another function to later
+
+    ! Why?  Makes code reusable and split up
+    !       Allows for 'Abstraction' to hide the detail in the code
+    !       transformer wants to change a string, but it doesn't care how it is changed, the 'HOW' is the detail in the abstracted callback functions
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// create higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string ${str}`);
+  console.log(`Transformed string ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord); // * callback function (inside transformer fn(str))
+console.log('-----');
+transformer('Javascript is the best!', oneWord); // * callback function
+
+// another call back function
+const high5 = function () {
+  console.log('✋');
+};
+document.body.addEventListener('click', high5); //eventListener is higher order function, and high5 is callback function
+
+// another
+['Jonas', 'Martha', 'Adam'].forEach(high5); //foreach item in array call back to function high5
