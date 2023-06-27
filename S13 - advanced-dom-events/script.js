ac54d540aaff6366378ca93b4b78b93bad55b73e
Brendan <b.campbell787@gmail.com>
2023-06-27 08:40:31 +0100

    S13 | EP 186: Selecting, Creating and Deleting elements

--- S13 - advanced-dom-events/script.js
+++ S13 - advanced-dom-events/script.js
@@ -30,3 +30,41 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// ! Selecting
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+const header = document.querySelector('.header'); //* find by class=value
+
+const allSections = document.querySelectorAll('.section'); // * returns a NodeList
+console.log(allSections);
+
+console.log(document.getElementById('section--1'));
+const allButtons = document.getElementsByTagName('button'); // * returns a HTMLCollection
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// ! Creating
+const message = document.createElement('div'); //not in the DOM yet, needs to be inserted
+message.classList.add('cookie-message')
+message.textContent = 'We use cookies for function and analytics'
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+
+// ! Inserting
+header.prepend(message) //show at top of header
+// header.append(message) //show at bottom of header
+// only append is applied because an element cannot be in two places at once
+
+// Alternative to prepend and append is to insert it before or after an element - 
+// outside of that element as a sibling instead of a child
+// header.before(message)
+// header.after(message.cloneNode(true))
+
+// ! Deleting
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+  message.remove()
+})
