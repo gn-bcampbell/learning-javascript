e0718e2cbed2d3a79c3afd539415c12d0a8a720f
Brendan <b.campbell787@gmail.com>
2023-07-15 15:18:05 +0100

    S13 | EP 209: Prototypes and Inheritence

--- S14 - oop/script.js
+++ S14 - oop/script.js
@@ -17,7 +17,7 @@ const Person = function (firstName, birthYear) {
 
     // create two new instance properties
     this.firstName = firstName,
-    this.birthYear = birthYear
+        this.birthYear = birthYear
 }
 
 // create any number of objects
@@ -29,3 +29,36 @@ const jack = new Person('Jack', 1995)
 console.log(matilda, jack)
 
 console.log(brendan instanceof Person) // true
+
+
+/*
+    ! S13 | EP 209: Prototypes
+
+    Basic inheritence
+    - Any object has access to the methods or properties from its prototype
+    - Effectively prototype of linked objects 
+    - Inherited properties go in __proto__ (they are available to inherited objects)
+    - Defined properties (name and birthyear) from the constuctor are applied to the created objects
+*/
+
+console.log(Person.prototype);
+
+// DOES NOT WORK WITH ARROW FUNCTIONS BECAUSE OF HOW 'THIS' KEYWORD OPERATES
+Person.prototype.calcAge = function () { console.log(2037 - this.birthYear); } // uses birthyear from Person instance constructor
+
+// reuse the object on instances of Person without having to repeat it on all objects.
+// this keyword is set to the object calling the method (so brendan result is different from Matilda)
+brendan.calcAge();
+matilda.calcAge();
+
+// ? Person.prototype is not the prototype of Person, instead it is what's going to be used as the prototype for any object
+// ? that is created by the Person constructor function (line 15)
+console.log(brendan.__proto__);
+console.log(brendan.__proto__ === Person.prototype); // true
+console.log(Person.prototype.isPrototypeOf(matilda)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// ? Where does __proto__ come from
+// ? It is step 3 from above, it sets its value to the prototype property of the function being called (Person)
+
+Person.prototype.species = 'Homo Sapiens' //now gets applied to the __proto__ of all objects (brendan, jack, matilda)
