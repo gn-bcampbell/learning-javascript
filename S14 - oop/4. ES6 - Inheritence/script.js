a4384417b849e5ce389f0fae6927550cbe8403d6
Brendan <b.campbell787@gmail.com>
2023-07-16 18:53:38 +0100

    S14 | EP 219 - 226: Fixing files, kinda janked everything about...

--- S14 - oop/4. ES6 - Inheritence/script.js
+++ S14 - oop/4. ES6 - Inheritence/script.js
@@ -36,3 +36,155 @@ Student.prototype.introduce = function () {
 const mike = new Student('Mike', 2020, 'Computer Science')
 mike.introduce();
 mike.calcAge(); // here we're doing a method lookup on the protoypal chain, it's not on Student object so it goes up to Person and finds it inside prototype
+
+
+
+/*
+    S13 | EP 220: ES6 inheritence and `super()` keyword
+*/
+
+class Person1 {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Instance methods
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet() {
+        console.log(`Hi ${this.fullName}`);
+    }
+
+    get age() {
+        return 2037 - this.birthYear;
+    }
+
+    set fullName(name) {
+        if (name.includes(' ')) this._fullName = name
+        else console.log('This is not a fullname');
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+
+    static hey() {
+        console.log(`Hey there 👋`);
+    }
+}
+
+class Student1 extends Person1 {
+
+    constructor(fullName, birthYear, course) {
+        super(fullName, birthYear) // ! constructor of Person class - needs to happen first
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`Hi there, I am ${this.fullName} and I'm studying ${this.course}`);
+    }
+
+    // ! override parent class method
+    calcAge() {
+        console.log(`Here we're overriding calcAge`);
+    }
+}
+
+const brendan = new Student1('Brendan C', 1993, 'Software Engineeering');
+
+brendan.introduce();
+brendan.calcAge();
+brendan.hey; // child classes do not inherit static methods from parents
+
+
+/*
+    S13 |   EP 222 : Class example 
+        |   EP 223 : Encapsulation
+        |   EP 224 : Private Class Fields and Methods
+        |   EP 225 : Chaining methods
+
+    ? Default values into constructor
+    ? Calling instance methods inside other instance methods (deposit/withdraw)
+    ? _underscore convention to denote private/readonly fields.
+
+    ? public fields     ->  create a class variable
+    ? private fields    ->  prefix with #
+    ? public methods    ->  standard ES6 methods 
+    ? private methods   ->  method prefix with #
+    ? static            ->  applies to all public/private fields/methods
+
+    ? `return this` in methods that set a value allows them to be chainable
+*/
+
+
+class Account {
+    // ! public fields (instances)
+    locale = navigator.language;
+    // ! private fields
+    #movements = [];
+
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        // *  add default values via constructor without having to pass them in
+        // this.locale = navigator.language;
+
+        // * protected _ is a convention - can be edited, but is a convention to inform others that it shouldn't be
+        // this._movements = []
+        this._pin = pin;
+
+        console.log(`Thanks for opening an account, ${this.owner}`);
+    }
+
+    // ! public methods
+
+    getMovements() {
+        return this.#movements;
+    }
+
+    deposit(value) {
+        this.#movements.push(value)
+        return this;    //! allows method to be chainable
+    }
+
+    withdraw(value) {
+        this.deposit(-value)
+        return this;
+    }
+
+    // _ denotes readOnly
+    _approveLoan() {
+        return true;
+    }
+
+    requestLoan(val) {
+        if (this._approveLoan) {
+            this.deposit(val)
+            console.log(`Loan approved`)
+            return this;
+        }
+    }
+
+    // ! private methods
+    #approvedInformation() {
+        return false;
+    }
+}
+
+const account1 = new Account('Brendan', 'EUR', 1111)
+console.log(account1);
+account1.deposit(444)
+account1.withdraw(123)
+account1.requestLoan(1000);
+// console.log(account1.#movements); // ! fails becuase it's a private field
+// console.log(account1.#approvedInformation()); // ! fails becuase it's not implemented yet in JavaScript
+console.log(account1);
+
+
+// use chaining via return this
+// without return this - it would be undefined because nothing gets returned
+account1.deposit(300).deposit(999).withdraw(909).requestLoan(1000)
+console.log(account1.getMovements());
--- S14 - oop/challenges/3/script.js
+++ S14 - oop/challenges/3/script.js
@@ -1,139 +1,66 @@
 'use strict';
 
-/*
-    S13 | EP 220: ES6 inheritence and `super()` keyword
+/* 
+    Your tasks:
+    1.  Use a constructor function to implement an Electric Car (called 'EV') as a child
+        "class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+        current battery charge in % ('charge' property)
+    
+    2.  Implement a 'chargeBattery' method which takes an argument
+        'chargeTo' and sets the battery charge to 'chargeTo'
+    
+    3.  Implement an 'accelerate' method that will increase the car's speed by 20,
+        and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
+        km/h, with a charge of 22%'
+    
+    4.  Create an electric car object and experiment with calling 'accelerate',
+        'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
+        you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+    
+    Test data:
+    - Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
 */
 
-class Person {
-    constructor(fullName, birthYear) {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
-    }
-
-    // Instance methods
-    calcAge() {
-        console.log(2037 - this.birthYear);
-    }
-
-    greet() {
-        console.log(`Hi ${this.fullName}`);
-    }
-
-    get age() {
-        return 2037 - this.birthYear;
-    }
-
-    set fullName(name) {
-        if (name.includes(' ')) this._fullName = name
-        else console.log('This is not a fullname');
-    }
-
-    get fullName() {
-        return this._fullName;
-    }
-
-    static hey() {
-        console.log(`Hey there 👋`);
-    }
+// * Create the constructor function of the parent object
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
 }
 
-class Student extends Person {
-
-    constructor(fullName, birthYear, course) {
-        super(fullName, birthYear) // ! constructor of Person class - needs to happen first
-        this.course = course;
-    }
-
-    introduce() {
-        console.log(`Hi there, I am ${this.fullName} and I'm studying ${this.course}`);
-    }
-
-    // ! override parent class method
-    calcAge() {
-        console.log(`Here we're overriding calcAge`);
-    }
+// * Create methods attached to prototype of parent function
+Car.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+    console.log(`${this.make} battery is charged to ${this.charge}%`);
 }
 
-const brendan = new Student('Brendan C', 1993, 'Software Engineeering');
-
-brendan.introduce();
-brendan.calcAge();
-brendan.hey; // child classes do not inherit static methods from parents
-
-
-/*
-    S13 |   EP 222 : Class example 
-        |   EP 223 : Encapsulation
-        |   EP 224 : Private Class Fields and Methods
-
-    ? Default values into constructor
-    ? Calling instance methods inside other instance methods (deposit/withdraw)
-    ? _underscore convention to denote private/readonly fields.
-
-    ? public fields     ->  create a class variable
-    ? private fields    ->  prefix with #
-    ? public methods    ->  standard ES6 methods 
-    ? private methods   ->  method prefix with #
-    ? static            ->  applies to all public/private fields/methods
-*/
-
-
-class Account {
-    // ! public fields (instances)
-    locale = navigator.language;
-    // ! private fields
-    #movements = [];
-
-    constructor(owner, currency, pin) {
-        this.owner = owner;
-        this.currency = currency;
-        // *  add default values via constructor without having to pass them in
-        // this.locale = navigator.language;
-
-        // * protected _ is a convention - can be edited, but is a convention to inform others that it shouldn't be
-        // this._movements = []
-        this._pin = pin;
-
-        console.log(`Thanks for opening an account, ${this.owner}`);
-    }
-
-    // ! public methods
-
-    getMovements() {
-        return this.#movements;
-    }
-
-    deposit(value) {
-        this.#movements.push(value)
-    }
-
-    withdraw(value) {
-        this.deposit(-value)
-    }
-
-    // _ denotes readOnly
-    _approveLoan() {
-        return true;
-    }
-
-    requestLoan(val) {
-        if (this._approveLoan) {
-            this.deposit(val)
-            console.log(`Loan approved`)
-        }
-    }
-
-    // ! private methods
-    #approvedInformation() {
-        return false;
-    }
+Car.prototype.brake = function () {
+    this.speed -= 20;
 }
 
-const account1 = new Account('Brendan', 'EUR', 1111)
-console.log(account1);
-account1.deposit(444)
-account1.withdraw(123)
-account1.requestLoan(1000);
-// console.log(account1.#movements); // ! fails becuase it's a private field
-// console.log(account1.#approvedInformation()); // ! fails becuase it's not implemented yet in JavaScript
-console.log(account1); 
+Car.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+}
+
+// * Create new EV object, inheriting constructor properties from Parent object
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+}
+
+// ! Link the two object prototypes to allow child to access methods declared in parent prototype
+EV.prototype = Object.create(Car.prototype)
+
+// ! If tesla.accelerate is called, this one is called as it exists first in the chain.
+// EV.prototype.accelerate = function () {
+//     console.log(`Calling EV method`);
+// }
+
+const tesla = new EV('Tesla', 120, 23)
+tesla.accelerate()
+tesla.accelerate()
+tesla.brake()
+tesla.chargeBattery(90)
+tesla.accelerate()
