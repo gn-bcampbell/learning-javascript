21c23963700421683dca25e0ed0018d8a2721641
Brendan <b.campbell787@gmail.com>
2024-07-05 15:47:43 +0100

    S15 | EP 236: Toggle workout form component

--- S15 - mapty-app-geolocation/script.js
+++ S15 - mapty-app-geolocation/script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map, mapEvent;
 
 /*
     ! S15 | EP 233: Using Geolocation API
@@ -24,29 +24,54 @@ if(navigator.geolocation)
     const {latitude, longitude} = position.coords;
     const coords = [latitude, longitude];
 
-    // 'map' here is the element in which to display the leaflet map view
-    // 'L' is the namespace for leaflet's methods
-    const map = L.map('map').setView([latitude, longitude], 13);
+    /*
+    S15 | EP 234: Display leaflet map on screen
+      'map' here is the element in which to display the leaflet map view
+      'L' is the namespace for leaflet's methods
+     */
+
+    map = L.map('map').setView([latitude, longitude], 13);
 
     // tile layer method uses openStreetMap to populate its tiles.
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    map.on('click', function(mapEvent){
-      const { lat, lng } = mapEvent.latlng;
-
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(L.popup({maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className:'running-popup'}))
-        .setPopupContent('Workout')
-        .openPopup();
-    })},
+      // S15 | EP 236: Render workout input form
+      map.on('click', function(mapE){
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus(); //allows you to immediately start typing in field
+      });
+    },
     function(){
     alert('Could not get position')
+
   })
 
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  // clear input fields
+  inputDistance.value = '';
+  inputDuration.value = '';
+  inputCadence.value = '';
+
+  // S15 | EP 235: Display a styled marker on the map
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(L.popup({maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className:'running-popup'}))
+    .setPopupContent('Workout')
+    .openPopup();
+})
+
+// S15 | EP 236: Toggle elevation and cadence based on inputType
+inputType.addEventListener('change', function(e) {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
