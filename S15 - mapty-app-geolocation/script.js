7bdbd27f13d91d492bfc16fea078a1f7087229fc
Brendan <b.campbell787@gmail.com>
2024-07-05 11:55:47 +0100

    S15 | EP 233: Using Geolocation API

--- S15 - mapty-app-geolocation/script.js
+++ S15 - mapty-app-geolocation/script.js
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+/*
+    ! S15 | EP 233: Using Geolocation API
+
+    Get current exact co-ordinates using the navigator object.
+    - Call navigator.geolocation.getCurrentPosition and pass in two callback functions
+    - one for success, one for error
+*/
+if(navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(function(position){
+    const {latitude, longitude} = position.coords;
+    console.log(`https://www.google.pt/maps/@${latitude}${longitude}`);
+  }, function(){
+    alert('Could not get position')
+  })
