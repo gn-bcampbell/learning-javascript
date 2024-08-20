80e326e4cc6a9e89943b6b210b6d208d10b3a500
Brendan <b.campbell787@gmail.com>
2024-08-20 15:15:18 +0100

    S16 | EP 251: Welcome to Callback Hell

--- S16 - async-ajax-await/script.js
+++ S16 - async-ajax-await/script.js
@@ -5,6 +5,9 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+/*
+    ! S16 | EP 245-250: Setup + implement XMLHttpRequest on API
+*/
 const renderCountry = function (data, className = '') {
 
     const name = data.name.common;
@@ -31,7 +34,9 @@ const renderCountry = function (data, className = '') {
     countriesContainer.style.opacity = 1;
 };
 
-
+/*
+    ! S16 | EP 245-250: Setup + implement XMLHttpRequest on API
+*/
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -66,4 +71,45 @@ const getCountryData = function (country) {
     });
 };
 
-getCountryData('portugal');
+// getCountryData('portugal');
+
+
+/*
+    ! S16 | EP 251: Welcome to Callback Hell
+
+    Create a function that ensures countries are called in order.
+    Effectively, have a callback function inside another callback function
+*/
+const getCountryAndNeighbour = function (country) {
+
+    // Ajax call 1
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        // Render country 1
+        renderCountry(data);
+
+        const [neighbour] = data.borders;
+        console.log(neighbour)
+
+        if (!neighbour) return
+
+        const request2 = new XMLHttpRequest()
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+        request2.send();
+        request2.addEventListener('load', function () {
+            const [data2] = JSON.parse(this.responseText);
+            console.log(data2);
+
+            // Render country 2
+            renderCountry(data2);
+        })
+    });
+};
+
+getCountryAndNeighbour('usa');
